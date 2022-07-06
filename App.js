92bae8fece948a4b7240672afa9f15454cb17a9e
Kerem Esen <kerem.esen.01@hotmail.com>
2022-07-06 22:33:58 +0300

    hiding firebase configs

--- App.js
+++ App.js
@@ -13,14 +13,22 @@ import { Provider, DefaultTheme } from "react-native-paper";
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+} from "@env";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDP6Z2aNNUrkGScSyPfpmz3s1RwC63iFQA",
-  authDomain: "chatty-ade5b.firebaseapp.com",
-  projectId: "chatty-ade5b",
-  storageBucket: "chatty-ade5b.appspot.com",
-  messagingSenderId: "1056687773888",
-  appId: "1:1056687773888:web:d7632dcd3dee6fbc847525",
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
 };
 
 !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
